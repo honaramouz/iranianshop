61e2f41347877987236afcf0c8b65176245c58eb
Nader Yari <hi@naderyari.ir>
2021-05-08 13:52:52 +0430

    تغییرات در فایل ایندکس و اضافه کردن فایل هدر و فوتر در پوشه اینکلودز فایل ایندکس ویرایش و قسمت  هدر و فوتر آن جدا شد فایل هدر به پوشه اینکلودز و فایل فوتر نیز به پوشه اینکلودز منتقل شد تا کد نویسی برنامه مرتب تر شود

--- index.php
+++ index.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" > 
-        <title>فروشگاه ایرانیان</title>
-        <link rel="stylesheet" href="css/style.css">
-        <style type="text/css">
-            
-            .set_style_link{
-                text-decoration: none;
-                font-weight: bold;
-            }
-            
-        </style>
-    </head>
-    <body>
-        <div class="divTable">
-            <div class="divTableRow">
-                <div class="divTableCell">
-                    <header class="divTable">
-                        <div class="divTableRow">
-                            <div class="divTableCell">لوگوی سایت</div>
-                        </div>
-                    </header>
-                    <nav class="divTable">
-                        <ul class="divTableRow">
-                            <li class="divTableCell"><a href="index.php">صفحه اصلی</a></li>
-                            <li class="divTableCell"><a href="">عضویت در سایت</a></li>
-                            <li class="divTableCell"><a href="">ورود به سایت</a></li>
-                            <li class="divTableCell"><a href="">درباره ما</a></li>
-                            <li class="divTableCell"><a href="">ارتباط با ما</a></li>
-                        </ul>
-                    </nav>
-                    <section class="divTable">
-                        <section class="divTableRow">
-                            <aside class="divTableCell" style="width: 25%;">بخش امکانات سایت</aside>
-                            <section class="divTableCell" style="width: 75%;">محصولات</section>
-                        </section>
-                    </section>
-                    <footer class="divTable">
-                        <section class="divTableRow">
-                            <article class="divTableCell">مالکیت مادی و معنوی سایت</article>
-                        </section>
-                    </footer>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+<?php
+include ("includes/header.php")
+?>
+                            محصولات
+<?php
+include ("includes/footer.php")
+?>
+                        
